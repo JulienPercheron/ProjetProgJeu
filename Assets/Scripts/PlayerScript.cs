e3ad4e670fa95ddfd18b741bf2fc8ad8b51a4b8b
JulienPercheron <julien_p@icloud.com>
2022-11-10 11:33:26 +0100

    Fix jump

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -6,6 +6,11 @@ public class PlayerScript : MonoBehaviour
 {
     public float speed = 5.0f;
     public float jumpHeight = 5.0f;
+    
+
+    bool jumping;
+    bool grounded;
+    float horizontalMovement;
 
     // Start is called before the first frame update
     void Start()
@@ -13,15 +18,43 @@ public class PlayerScript : MonoBehaviour
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        float x = this.transform.position.x + Input.GetAxis("Horizontal") * Time.deltaTime * speed;
-        this.transform.position = new Vector3(x, this.transform.position.y, this.transform.position.z);
-
         if (Input.GetButtonDown("Jump"))
         {
+            jumping = true;
+        }
+        if (Input.GetButtonUp("Jump"))
+        {
+            jumping = false;
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        horizontalMovement = this.transform.position.x + Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+        this.transform.position = new Vector3(horizontalMovement, this.transform.position.y, this.transform.position.z);
+
+        if (jumping && grounded)
+        {
             this.GetComponent<Rigidbody>().AddForce(0.0f, jumpHeight, 0.0f, ForceMode.Impulse);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Sol")
+        {
+            grounded = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Sol")
+        {
+            grounded = false;
         }
     }
 }
